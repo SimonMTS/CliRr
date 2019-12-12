d28f4bede614da72e30a92254b9bb84d4db7d819
SimonMTS <info@simonstriekwold.nl>
2019-12-12 20:27:25 +0100

    attempt to highlight song names

--- src/view.rs
+++ src/view.rs
@@ -57,7 +57,11 @@ fn song_list(status: &Status, lock: &mut StdoutLock) {
             write!(lock, " ").expect("stdout err");
         }
 
-        writeln!(lock, "  \x1B[36m[\x1B[37m{}\x1B[36m]\x1B[0m {}", i+1, split_song[1]).expect("stdout err");
+        if split_song[1].contains("-") {
+            writeln!(lock, "  \x1B[36m[\x1B[37m{}\x1B[36m]\x1B[0m {}\x1B[0m", i+1, split_song[1].replacen("-", "-\x1B[36m", 1).replacen("(", "\x1B[0m(", 1)).expect("stdout err");
+        } else {
+            writeln!(lock, "  \x1B[36m[\x1B[37m{}\x1B[36m]\x1B[0m \x1B[36m{}\x1B[0m", i+1, split_song[1]).expect("stdout err");
+        }
     }    
 
 }
